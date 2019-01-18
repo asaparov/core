1c7f78e6d1c32c5ca1f1ef1f1af33dd538a95d31
asaparov <abulhair.saparov@gmail.com>
2019-01-18 12:01:21 -0500

    Added a function to append a wide character to a multi-byte string to lex.h.

--- lex.h
+++ lex.h
@@ -387,6 +387,22 @@ bool expect_token(const array<lexical_token<TokenType>>& tokens,
 	return true;
 }
 
+/**
+ * Appends the given wide character `next` to the char array `token` which
+ * represents a multi-byte string.
+ */
+inline bool append_to_token(
+	array<char>& token, wint_t next, std::mbstate_t& shift)
+{
+	if (!token.ensure_capacity(token.length + MB_CUR_MAX))
+		return false;
+	size_t written = wcrtomb(token.data + token.length, next, &shift);
+	if (written == static_cast<std::size_t>(-1))
+		return false;
+	token.length += written;
+	return true;
+}
+
 } /* namespace core */
 
 #endif /* LEX_H_ */
